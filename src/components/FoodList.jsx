9bdda61fc9d8dbd7fa1df986c17a2f100e592dca
EbinReji18 <ebinofficial18@gmail.com>
2025-07-17 12:00:23 +0530

    modified FoodList

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,11 +4,15 @@ export default function FoodList({ foodData, setFoodId }) {
   return (
     <div className="container my-4">
       <div className="row g-4 justify-content-center">
-        {foodData.map((food) => (
-          <div key={food.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <FoodItem food={food} setFoodId={setFoodId} />
-          </div>
-        ))}
+        {Array.isArray(foodData) && foodData.length > 0 ? (
+          foodData.map((food) => (
+            <div key={food.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <FoodItem food={food} setFoodId={setFoodId} />
+            </div>
+          ))
+        ) : (
+          <div className="text-center text-muted">No recipes found.</div>
+        )}
       </div>
     </div>
   );
